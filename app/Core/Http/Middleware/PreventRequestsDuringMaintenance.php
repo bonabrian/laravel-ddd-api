08c650d4ab1ab9f77a86cd5022bd45335c2312fe
Bona Brian Siagian <bonabriansiagian@gmail.com>
2020-10-12 20:59:31 +0700

    PreventRequestsDuringMaintenance

---
+++ app/Core/Http/Middleware/PreventRequestsDuringMaintenance.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @author Bona Brian Siagian <bonabriansiagian@gmail.com>
+ */
+
+namespace App\Core\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance as Middleware;
+
+class PreventRequestsDuringMaintenance extends Middleware
+{
+    /**
+     * The URIs that should be reachable while maintenance mode is enabled.
+     *
+     * @var array
+     */
+    protected $except = [
+        //
+    ];
+}
